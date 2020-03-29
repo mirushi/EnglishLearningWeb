51ac361582e16c7b342e038e172b69fe3c40f7cd
nghi dep trai <luubieunghi@gmail.com>
2020-03-29 20:57:21 +0700

    Initial commit

---
+++ src/main/java/com/comittedpeople/englishlearningweb/EnglishlearningwebApplication.java
@@ -0,0 +1,13 @@
+package com.comittedpeople.englishlearningweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishlearningwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishlearningwebApplication.class, args);
+	}
+
+}
