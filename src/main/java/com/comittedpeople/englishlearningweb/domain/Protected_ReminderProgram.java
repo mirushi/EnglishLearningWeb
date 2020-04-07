3442bf2dcd32dd2e74e52f96e12687327c75f7fe
nghi dep trai <luubieunghi@gmail.com>
2020-04-08 00:44:51 +0700

    Update Protected_ReminderProgram.java

--- src/main/java/com/comittedpeople/englishlearningweb/domain/Protected_ReminderProgram.java
+++ src/main/java/com/comittedpeople/englishlearningweb/domain/Protected_ReminderProgram.java
@@ -1,11 +1,14 @@
 package com.comittedpeople.englishlearningweb.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Protected_ReminderProgram {
 	
+	@Id
 	private Long id;
+	
 	private String name;
 	
 }
